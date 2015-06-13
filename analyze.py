3d6ca30ddbb39b4c23fd0a0bdc3cafe7f3ae649e
Danny Hermes <daniel.j.hermes@gmail.com>
2015-06-13 16:53:16 -0700

    Converting 2014 analysis to cover different cohorts.
    Broken down by
    - All, 25-34, 35-54 and 55 and older.

--- analyze.py
+++ analyze.py
@@ -12,7 +12,7 @@ def to_int(val):
     return int(val.replace(',', ''))
 
 
-def parse_2014():
+def parse_2014(num_col, num_col_name):
     filename = os.path.join(TABLES_DIR, '2014_Table_3.csv')
     with open(filename, 'rb') as csvfile:
         reader = csv.reader(csvfile)
@@ -23,14 +23,15 @@ def parse_2014():
 
     # Check header row.
     headers = [
-        ['Detailed Years of School', 'ALL RACES', ''],
-        ['', 'Number', 'Percent'],
+        ['Detailed Years of School', num_col_name],
+        ['', 'Number'],
     ]
-    if not np.all(sheet[4:6, :3] == headers):
+    type_col = 0
+    if not np.all(sheet[4:6, [type_col, num_col]] == headers):
         raise ValueError('Unexpected row headers.')
 
-    schooling_types = sheet[6:, 0]
-    schooling_numbers = sheet[6:, 1]
+    schooling_types = sheet[6:, type_col]
+    schooling_numbers = sheet[6:, num_col]
 
     if schooling_types[0] != '':
         raise ValueError('Unexpected name for "total"')
@@ -66,31 +67,43 @@ def parse_2014():
                 to_int(schooling_numbers[index]),
             ))
 
-    if sum([val[-1] for val in result]) != total_people:
+    if np.abs(sum([val[-1] for val in result]) - total_people) > 5:
         raise ValueError('Total does not match observed.')
     return total_people, result
 
 
 def analyze_2014():
-    total_people, result = parse_2014()
+    options = [
+        (1, 'ALL RACES'),
+        (7, '25 TO 34  YEARS OLD'),
+        (9, '35 TO 54  YEARS OLD'),
+        (11, '55 YEARS OLD AND OVER'),
+    ]
+    result = []
+    for num_col, num_col_name in options:
+        pretty_name = ' '.join(num_col_name.split())
+        print '2014; %25s:' % (pretty_name,),
+        total_people, result = parse_2014(num_col, num_col_name)
 
-    # Assumes the data is in order of educational attainment from
-    # least (i.e. Less than 1st grade) to most (i.e. PhD)
+        # Assumes the data is in order of educational attainment from
+        # least (i.e. Less than 1st grade) to most (i.e. PhD)
 
-    # We expect exactly one match for each type.
-    begin_of_hs, = [row for row, value in enumerate(result)
-                    if value[0] == 'High school diploma']
-    begin_of_bachelors, = [row for row, value in enumerate(result)
-                           if value[0] == 'Bachelors degree only']
+        # We expect exactly one match for each type.
+        begin_of_hs, = [row for row, value in enumerate(result)
+                        if value[0] == 'High school diploma']
+        begin_of_bachelors, = [row for row, value in enumerate(result)
+                               if value[0] == 'Bachelors degree only']
 
-    hs_tot = sum([value[-1] for value in result[begin_of_hs:]])
-    bachelors_tot = sum([
-        value[-1] for value in result[begin_of_bachelors:]])
+        hs_tot = sum([value[-1] for value in result[begin_of_hs:]])
+        bachelors_tot = sum([
+            value[-1] for value in result[begin_of_bachelors:]])
 
-    total_people = float(total_people)
-    hs_percent = hs_tot / total_people
-    bachelors_percent = bachelors_tot / total_people
+        total_people = float(total_people)
+        hs_percent = hs_tot / total_people
+        bachelors_percent = bachelors_tot / total_people
 
-    print '2014: HS -> %4.1f and BA/BS -> %4.1f' % (
-        100 * hs_percent, 100 * bachelors_percent)
-    return hs_percent, bachelors_percent
+        print 'HS -> %4.1f and BA/BS -> %4.1f' % (
+            100 * hs_percent, 100 * bachelors_percent)
+        result.append((num_col_name, hs_percent, bachelors_percent))
+
+    return result
